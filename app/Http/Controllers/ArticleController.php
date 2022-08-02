3455fd04ed90051b0b72e9b146eabe6f5bc71cbb
jasterweb <web@jaster.co.id>
2022-08-02 11:12:02 +0700

    index artcle

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        $articles = Article::all();
+        return view('article.allpost',compact('articles'));
+    }
 }
