8806726b7fd0b7eab9b38225cc753a0903bc62b8
maulanafanny <maulanafanny38@gmail.com>
2022-08-02 16:14:32 +0700

    user datatable

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'username' => fake()->name(),
+            'username' => fake()->userName(),
+            'fullname' => fake()->name(),
+            'phone' => fake()->numerify('+62 ###-###-###'),
+            'address' => fake()->city(),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 namespace Database\Seeders;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
@@ -22,5 +23,7 @@ class UsersTableSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now()
         ]); */
+
+        User::factory(30)->create();
     }
 }
