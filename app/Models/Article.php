75a6e5018bc1da5117d78a2ccbc4638507fbd825
jasterweb <web@jaster.co.id>
2022-08-02 11:09:36 +0700

    fillable article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'desc',
+        'img',
+        'slug',
+        'published_at'
+    ];
 }
