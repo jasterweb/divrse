76daa0c6de7deba1be392cf7e577be233b117310
jasterweb <web@jaster.co.id>
2022-08-04 05:38:29 +0700

    Home#1

--- resources/views/layouts/footers/guest.blade.php
+++ resources/views/layouts/footers/guest.blade.php
@@ -1,5 +1,25 @@
-<footer class="py-5">
+<footer class="py-5 footlanding">
     <div class="container">
-        @include('layouts.footers.nav')
+        <div class="row align-items-center">
+            <div class="col-lg-6 col-md-6">
+                <h2 class="title2 text-white">Ready to stop talking about diversity recruitment and start doing it?</h2>
+                <span class="text-light desc1">Turn your DE&I goals into reality with Divrse’s quick, easy and affordable solution</span>
+                <br/>
+                <a href="#" class="btn btn-xl btn-secondary mt-5">Get Started</a>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="image-box imgcta">
+                    <img src="{{ asset('argon') }}/home/footcta.png">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyr">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-light">Copyright © 2022 Divrse - All Rights Reserved</div>
+                
+            </div>
+        </div>
     </div>
 </footer>
---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Home - {{ config('app.name') }}</title>
+        <!-- Favicon -->
+        <link href="{{ asset('argon') }}/logo/fav.png" rel="icon" type="image/png">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+        <!-- Extra details for Live View on GitHub Pages -->
+
+        <!-- Icons -->
+        <link href="{{ asset('argon') }}/vendor/nucleo/css/nucleo.css" rel="stylesheet">
+        <link href="{{ asset('argon') }}/vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+        <!-- Argon CSS -->
+        <link type="text/css" href="{{ asset('argon') }}/css/argon.css?v=1.0.0" rel="stylesheet">
+        <link type="text/css" href="{{ asset('argon') }}/css/home.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+        <script src="https://unpkg.com/dropzone@5/dist/min/dropzone.min.js"></script>
+        <link
+        rel="stylesheet"
+        href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css"
+        type="text/css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;800&display=swap" rel="stylesheet"> 
+
+    </head>
+    <body class="{{ $class ?? '' }}">
+       
+        <div class="main-content">
+            @include('layouts.navbars.navs.landing')
+            @yield('content')
+        </div>
+
+        @guest()
+            @include('layouts.footers.guest')
+        @endguest
+
+      
+        <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
+        <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+        @stack('js')
+        
+        <!-- Argon JS -->
+        <script src="{{ asset('argon') }}/js/argon.js?v=1.0.0"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                $('.wcuslide').slick({
+                    dots: true,
+                    infinite: true,
+                    arrows: false,
+                    speed: 500,
+                    slidesToShow: 3,         
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                    slidesToScroll: 1,
+                    responsive: [
+                        {
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            infinite: true,
+                            dots: true
+                        }
+                        },
+                        {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 2
+                        }
+                        },
+                        {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                        }
+                        // You can unslick at a given breakpoint now by adding:
+                        // settings: "unslick"
+                        // instead of a settings object
+                    ]
+                    });
+            });
+       </script>
+    </body>
+</html>
