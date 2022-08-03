844e23d76b6e449066822c690398aa11652a52ac
maulanafanny <maulanafanny38@gmail.com>
2022-08-03 15:14:29 +0700

    users create and delete using resource

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,7 +11,7 @@ class UserController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('permission:manage users', ['only' => ['index']]);
+        $this->middleware('permission:manage users', ['only' => ['index', 'create', 'store', 'update', 'destroy']]);
     }
 
     /**
@@ -24,5 +25,84 @@ class UserController extends Controller
         return view('users.index', [
             'users' => User::all()
         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'fullname' => 'required',
+            'address' => 'required',
+            'phone' => 'required'
+        ]);
+
+        User::create($validatedData);
+
+        return redirect('/user');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return redirect('/user');
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,28 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('layouts.headers.cards')
+    <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+        <div class="container-fluid">
+        </div>
+    </div>
 
-        <div class="container-fluid mt--7">
-            <div class="row">
-                <div class="col">
-                    <div class="card shadow">
-                        <div class="card-header border-0">
-                            <div class="row align-items-center">
-                                <div class="col-8">
-                                    <h3 class="mb-0">Users</h3>
-                                </div>
-                                <div class="col-4 text-right">
-                                    <a href="#" class="btn btn-sm btn-primary">Add user</a>
-                                </div>
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col">
+                <div class="card shadow mb-5">
+
+                    <div class="card-header border-0">
+                        <div class="row align-items-center">
+                            <div class="col-8">
+                                <h3 class="mb-0">Users</h3>
+                            </div>
+                            <div class="col-4 text-right">
+                                <a href="/user/create" class="btn btn-sm btn-primary">Add user</a>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="col-12">
-                        </div>
+                    <div class="card-body">
 
                         <div class="table-responsive">
                             <table id="users-table" class="table align-items-center table-flush">
+
                                 <thead class="thead-light">
                                     <tr>
                                         <th scope="col">No</th>
@@ -35,36 +39,48 @@
                                         <th scope="col"></th>
                                     </tr>
                                 </thead>
+
                                 <tbody>
+
                                     @foreach ($users as $user)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $user->username }}</td>
-                                        <td>{{ $user->fullname }}</td>
-                                        <td>{{ $user->address }}</td>
-                                        <td><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></td>
-                                        <td>{{ $user->phone }}</td>
-                                        <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
-                                        <td class="text-right">
-                                            <div class="dropdown">
-                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <i class="fas fa-ellipsis-v"></i>
-                                                </a>
-                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                    <a class="dropdown-item" href="">Edit</a>
-                                                    <a class="dropdown-item" href="">Delete</a>
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $user->username }}</td>
+                                            <td>{{ $user->fullname }}</td>
+                                            <td>{{ $user->address }}</td>
+                                            <td><a href="mailto:{{ $user->email }}">{{ $user->email }}</a></td>
+                                            <td>{{ $user->phone }}</td>
+                                            <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
+                                            <td class="text-right">
+                                                <div class="dropdown">
+                                                    <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                        <i class="fas fa-ellipsis-v"></i>
+                                                    </a>
+                                                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                        <a class="dropdown-item" href="">Edit</a>
+                                                        <form method="POST" action="/user/{{ $user->id }}">
+                                                            @csrf
+                                                            @method('delete')
+
+                                                            <button class="dropdown-item" type="submit">Delete</button>
+                                                        </form>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                            </td>
+                                        </tr>
                                     @endforeach
+
                                 </tbody>
+                                
                             </table>
                         </div>
-                        <div class="card-footer py-4">
-                            <nav class="d-flex justify-content-end" aria-label="..."></nav>
-                        </div>
+                        
                     </div>
+
+                    <div class="card-footer py-4">
+                        <nav class="d-flex justify-content-end" aria-label="..."></nav>
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -72,17 +88,17 @@
 @endsection
 
 @push('js')
-    <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script>
+    {{-- <script src="{{ asset('argon') }}/vendor/jquery/dist/jquery.min.js"></script> --}}
     {{-- <script src="{{ asset('argon') }}/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script> --}}
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
 
     <!-- Argon JS -->
-    <script src="{{ asset('argon') }}/js/argon.js?v=1.0.0"></script>
+    {{-- <script src="{{ asset('argon') }}/js/argon.js?v=1.0.0"></script> --}}
 
     <script>
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('#users-table').DataTable();
         });
     </script>
