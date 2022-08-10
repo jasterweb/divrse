be787ab5b709f5bd28c5cf66f8bd1478394a57a1
maulanafanny <maulanafanny38@gmail.com>
2022-08-11 02:29:49 +0700

    talent & solution page responsiveness

--- resources/views/talent.blade.php
+++ resources/views/talent.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="header bg-primary pt-9 pb-6">
         <div class="container">
-            <div class="header-body mt-4 mb-4">
+            <div class="header-body my-4">
                 <div class="row align-items-center">
                     <div class="col-lg-6 col-md-6">
                         <h1 class="titlesect text-left text-white">Hire Diverse, Top Talent</h1>
@@ -11,7 +11,7 @@
                         <span class="desc1 text-light">With Divrse’s AI-powered recruiting automation platform, you’ll set your team
                             up for success by hiring diverse, qualified talent year round.</span>
                         <br/>
-                        <a href="#" class="btn btn-xl btn-secondary mt-4">Get Started</a>
+                        <a href="#" class="btn btn-xl btn-secondary mt-4 mb-md-0 mb-4">Get Started</a>
                     </div>
                     <div class="col-lg-6 col-md-6">
                         <div class="image-box heroimage">
@@ -32,7 +32,7 @@
                
                 <div class="col-lg-6 col-md-6">
              
-                        <h2 class="title3 text-left">We do the <b>Sourcing</b> for you</h2>
+                        <h2 class="title3 text-left mb-md-0 mb-3">We do the <b>Sourcing</b> for you</h2>
                         <p class="text-dark">Divrse is an automated diversity recruiting layer that allows you to spend less time sourcing random potential candidates and more time building meaningful interactions. Every candidate in Divrse’s pool is qualified, diverse, and ready for the job.
                             Divrse increases the number of underrepresented candidates that apply to your jobs while also providing you with applicants best fit for the role.</p>
                         
@@ -63,7 +63,7 @@
                 </div>
                 <div class="col-lg-7 col-md-7">
              
-                        <h2 class="title3 text-left">Build <b>Authentic</b> connections with <b>qualified</b> Talent</h2>
+                        <h2 class="title3 text-left my-md-0 my-3">Build <b>Authentic</b> connections with <b>qualified</b> Talent</h2>
                         <p class="text-light">Increase your outbound recruiting response rates by connecting with our highly engaged talent pool. Manage conversations
                             at scale by leveraging our suite of recruiting tools such as messaging, events, and communities to build authentic connections.</p>
                         
@@ -80,26 +80,26 @@
             <div class="row align-items-center">
                 <div class="col-md-12">
                     <h2 class="titlesect">The Divrse Journey</h2>
-                    <div class="stepby">
-                        <div class="itemstep">
+                    <div class="stepby row">
+                        <div class="itemstep col-md col-6">
                             <div class="wrapstep">
                                 <img src="{{ asset('argon') }}/home/journey.png">
                                 <span>Connect Divrse</span>
                             </div>
                         </div>
-                        <div class="itemstep">
+                        <div class="itemstep col-md col-6 mb-md-0 mb-5">
                             <div class="wrapstep">
                                 <img src="{{ asset('argon') }}/home/journey.png">
                                 <span>Share Positions</span>
                             </div>
                         </div>
-                        <div class="itemstep">
+                        <div class="itemstep col-md col-6">
                             <div class="wrapstep">
                                 <img src="{{ asset('argon') }}/home/journey.png">
                                 <span>Expose Opportunity</span>
                             </div>
                         </div>
-                        <div class="itemstep">
+                        <div class="itemstep col-md col-6">
                             <div class="wrapstep">
                                 <img src="{{ asset('argon') }}/home/journey.png">
                                 <span>Get Talent</span>
@@ -138,7 +138,7 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-12 col-md-12">
-                    <h2 class="titlesect">Seamless ATS Integration</h2>
+                    <h2 class="titlesect mb-md-0 mb-4">Seamless ATS Integration</h2>
                     <p class="text-dark m800">Divrse easily connects to your ATS provider. Gain new insights about talent already in your pipeline and increase productivity.</p>
                     <ul class="atslogo">
                         <li class="itemats"><img src="{{ asset('argon') }}/home/ats1.png"></li>
@@ -156,7 +156,7 @@
     <section class="bg-blue pt-6 pb-6" style="background-image: url('{{ asset('argon') }}/home/bgtalent.png');background-size: cover;background-position: center;">
         <div class="container">
             <div class="row">
-                <div class="o-center">
+                <div class="o-center mx-md-0 mx-3">
                     <h2 class="titlesect text-white">Automated, People-driven diversity sourcing</h2>
                     <p class="text-light m800">Divrse combines machine learning with human insights to make sure the pooled candidates reflect real diversity of demographic, backgrounds, and skills.</p>
                 </div>
@@ -167,7 +167,7 @@
     <section class="bg-white pt-6 pb-6">
         <div class="container">
             <div class="row align-items-center">
-               <div class="col-lg-6 col-md-6">
+               <div class="col-lg-6 col-md-6 pb-md-0 pb-4">
                     <div class="image-box introimg">
                         <img src="{{ asset('argon') }}/home/intro1.png">
                     </div>
@@ -178,15 +178,16 @@
                     <p class="text-dark">Get Intentional with your diversity hiring and sourcing efforts. Startup to Fortune 500 companies are using diversity hiring practices to source
                         historically marginalized talent, rethink job qualifications and enhance employer branding.
                     </p>
-                    
-                    <a href="#" class="btn btn-xl btn-secondary mt-4">Get Started</a>
+                    <div class="center-small">
+                        <a href="#" class="btn btn-xl btn-secondary mt-4">Get Started</a>
+                    </div>
                 </div>
             </div>
         </div>
     </section>
 
 
-    <footer class="py-5 footlanding">
+    <footer class="py-5 footlanding center-small">
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-6">
